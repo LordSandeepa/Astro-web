9c8e39a75ebc8c182cf6e5721bfd658996728aa2
Sandeepa Bandara <108658788+LordSandeepa@users.noreply.github.com>
2024-07-20 10:25:31 +0530

    ss

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,33 +6,44 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
-
 import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/LordSandeepa/Sandeepa-web",
+  site: "https://github.com/LordSandeepa/Astro-web/",
+  base: "/Astro-web/",
   prefetch: true,
   markdown: {
     ...markdownConfig
   },
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react(), mdx({
-    ...markdownConfig,
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "one-dark-pro"
-    }
-  }), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), robotsTxt({
-    sitemap: ['https://sandeepa-net.netlify.app/sitemap-0.xml', 'https://sandeepa-net.netlify.app/sitemap-index.xml']
-  }), playformCompress()]
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }),
+    react(),
+    mdx({
+      ...markdownConfig,
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "one-dark-pro"
+      }
+    }),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    }),
+    robotsTxt({
+      sitemap: [
+        'https://sandeepa-net.netlify.app/sitemap-0.xml',
+        'https://sandeepa-net.netlify.app/sitemap-index.xml'
+      ]
+    }),
+    playformCompress()
+  ]
   // output: "server",
   // adapter: vercel({
   //   webAnalytics: {
